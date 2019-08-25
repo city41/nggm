facd79c79c89afd728dfc3a4d3260cb0ec22d68b
Matt Greer <matt.e.greer@gmail.com>
2019-08-25 10:53:51 -0700

    Updates

---
+++ precache-manifest.2b9d19b1ce55433f427cec69f9390e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a326e35c2d3ea1a8cdb180d570f6248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22273d1a11a684cc97ad",
+    "url": "/static/css/main.6e258020.chunk.css"
+  },
+  {
+    "revision": "a54b59f42153a32cabb9",
+    "url": "/static/js/2.50c3a95d.chunk.js"
+  },
+  {
+    "revision": "22273d1a11a684cc97ad",
+    "url": "/static/js/main.b4ec5d4c.chunk.js"
+  },
+  {
+    "revision": "a8d12bedcad58e568d45",
+    "url": "/static/js/runtime~main.8201726a.js"
+  }
+]);
