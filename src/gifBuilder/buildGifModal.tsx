80218915c70f506ec697d00c76c4451b080aeef7
Matt Greer <matt.e.greer@gmail.com>
2019-09-04 21:50:27 -0700

    Removed omggif

--- src/gifBuilder/buildGifModal.tsx
+++ src/gifBuilder/buildGifModal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Modal from "react-modal";
-import { createGif } from "../state/createGif_jsgif";
+import { createGif } from "../state/createGif";
 import { useAppState } from "../state";
 import { ExtractedSpriteGroup } from "../state/types";
 
