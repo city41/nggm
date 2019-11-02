c57e64e7996e128499139ea08ea315d5c3fd2c5b
Matt Greer <matt.e.greer@gmail.com>
2019-11-02 14:46:37 -0700

    gatsby-browser: Modal.setAppElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import Modal from "react-modal";
 
 import "./src/index.css";
+
+export function onClientEntry() {
+  Modal.setAppElement(document.getElementById("___gatsby"));
+}
