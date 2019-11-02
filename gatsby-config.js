2cfd1016d207c882a96ac54280c9ef35abf1b937
Matt Greer <matt.e.greer@gmail.com>
2019-11-02 15:03:52 -0700

    gatsby-config: pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/ngbg",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
